8820eb1f0cb03ec4a49033cbfba2c267e092faca
gbl08ma <gbl08ma@gmail.com>
2017-10-11 13:32:14 +0100

    Fix schedule comparison
    Consider two schedules to be the same if both are closed

--- dataobjects/schedule.go
+++ dataobjects/schedule.go
@@ -71,7 +71,10 @@ func getSchedulesWithSelect(node sqalx.Node, sbuilder sq.SelectBuilder) ([]*Sche
 
 // Compare checks if two schedules are the same regardless of their day
 func (schedule *Schedule) Compare(s2 *Schedule) bool {
-	return schedule.Open == s2.Open && schedule.OpenTime == s2.OpenTime && schedule.OpenDuration == s2.OpenDuration
+	return (schedule.Open == false && s2.Open == false) ||
+		(schedule.Open == s2.Open &&
+			schedule.OpenTime == s2.OpenTime &&
+			schedule.OpenDuration == s2.OpenDuration)
 }
 
 // Update adds or updates the schedule
