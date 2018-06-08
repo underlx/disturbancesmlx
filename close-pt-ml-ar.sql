bf24964a33bc39345180ae3d0b8b07fe9b623418
gbl08ma <gbl08ma@gmail.com>
2018-06-08 20:27:12 +0100

    We have more table columns than we used to...

--- close-pt-ml-ar.sql
+++ close-pt-ml-ar.sql
@@ -4,6 +4,6 @@ UPDATE station_lobby_schedule SET open = false WHERE lobby_id LIKE 'pt-ml-ar-%';
 DELETE FROM connection WHERE from_station = 'pt-ml-an' AND to_station = 'pt-ml-ar';
 DELETE FROM connection WHERE from_station = 'pt-ml-am' AND to_station = 'pt-ml-ar';
 -- connect pt-ml-an directly to pt-ml-am
-INSERT INTO connection VALUES ('pt-ml-an', 'pt-ml-am', 0), ('pt-ml-am', 'pt-ml-an', 0);
+INSERT INTO connection VALUES ('pt-ml-an', 'pt-ml-am', 0, 0, 0, 0), ('pt-ml-am', 'pt-ml-an', 0, 0, 0, 0);
 UPDATE dataset_info SET version = now() WHERE network_id = 'pt-ml';
 COMMIT;
