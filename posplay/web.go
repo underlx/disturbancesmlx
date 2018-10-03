1e9e233482fcd96813aac01300a8b747c4dcf3f4
gbl08ma <gbl08ma@gmail.com>
2018-10-03 12:04:55 +0100

    PosPlay: implement all-time leaderboards

--- posplay/web.go
+++ posplay/web.go
@@ -45,6 +45,8 @@ func ConfigureRouter(router *mux.Router) {
 	router.HandleFunc("/pair/status", pairStatus)
 	router.HandleFunc("/settings", settingsPage)
 	router.HandleFunc("/leaderboards", leaderboardsPage)
+	router.HandleFunc("/leaderboards/weekly", leaderboardsPage)
+	router.HandleFunc("/leaderboards/alltime", leaderboardsAllTimePage)
 	router.HandleFunc("/login", forceLogin)
 	router.HandleFunc("/logout", forceLogout)
 	router.HandleFunc("/oauth/callback", callbackHandler)
@@ -473,6 +475,73 @@ func leaderboardsPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = webtemplate.ExecuteTemplate(w, "leaderboards.html", p)
+	if err != nil {
+		config.Log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func leaderboardsAllTimePage(w http.ResponseWriter, r *http.Request) {
+	session, redirected, err := GetSession(r, w, true)
+	if err != nil {
+		config.Log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if redirected {
+		return
+	}
+
+	tx, err := config.Node.Beginx()
+	if err != nil {
+		config.Log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer tx.Commit() // read-only
+
+	discordID := uidConvS(session.DiscordInfo.ID)
+
+	player, err := dataobjects.GetPPPlayer(tx, discordID)
+	if err != nil {
+		config.Log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	p := struct {
+		pageCommons
+		Leaderboard struct {
+			Entries []dataobjects.PPLeaderboardEntry
+		}
+	}{}
+	p.pageCommons, err = initPageCommons(tx, w, r, "Classificações globais", session, player)
+	if err != nil {
+		config.Log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	p.SidebarSelected = "leaderboards"
+
+	start := time.Time{}
+	end := time.Now()
+	entries, err := dataobjects.PPLeaderboardBetween(tx, start, end, 50, player)
+	if err != nil {
+		config.Log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	if len(entries) == 1 && entries[0].Position == 0 {
+		// avoid showing just this player in the 0th place
+		entries = []dataobjects.PPLeaderboardEntry{}
+	}
+
+	p.Leaderboard = struct {
+		Entries []dataobjects.PPLeaderboardEntry
+	}{
+		Entries: entries,
+	}
+
+	err = webtemplate.ExecuteTemplate(w, "leaderboards-alltime.html", p)
 	if err != nil {
 		config.Log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
