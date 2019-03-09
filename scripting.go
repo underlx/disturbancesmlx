19e215fa31e9a64bc685bf2437a257d6aa7bbd7b
gbl08ma <gbl08ma@gmail.com>
2019-03-09 21:45:08 +0000

    Make more code available to the scripting system

--- scripting.go
+++ scripting.go
@@ -9,25 +9,12 @@ import (
 	"github.com/underlx/disturbancesmlx/dataobjects"
 	"github.com/underlx/disturbancesmlx/discordbot"
 	"github.com/underlx/disturbancesmlx/posplay"
+	"github.com/underlx/disturbancesmlx/resource"
 	"github.com/underlx/disturbancesmlx/utils"
 	"github.com/underlx/disturbancesmlx/website"
 )
 
 func ankoPackageConfigurator(packages, packageTypes map[string]map[string]interface{}) {
-	packages["underlx"] = make(map[string]interface{})
-	packages["underlx"]["RootSqalxNode"] = func() sqalx.Node {
-		return rootSqalxNode
-	}
-	packages["underlx"]["VehicleHandler"] = func() *compute.VehicleHandler {
-		return vehicleHandler
-	}
-	packages["underlx"]["StatsHandler"] = func() *compute.StatsHandler {
-		return statsHandler
-	}
-	packages["underlx"]["ReportHandler"] = func() *compute.ReportHandler {
-		return reportHandler
-	}
-
 	type pkgInfo struct {
 		Name      string
 		Types     map[string]reflect.Type
@@ -67,12 +54,27 @@ func ankoPackageConfigurator(packages, packageTypes map[string]map[string]interf
 	processPkg(pkgInfo{"compute", compute.Types, compute.Functions, compute.Consts, compute.Variables})
 	processPkg(pkgInfo{"dataobjects", dataobjects.Types, dataobjects.Functions, dataobjects.Consts, dataobjects.Variables})
 	processPkg(pkgInfo{"discordbot", discordbot.Types, discordbot.Functions, discordbot.Consts, discordbot.Variables})
+	processPkg(pkgInfo{"resource", resource.Types, resource.Functions, resource.Consts, resource.Variables})
 	processPkg(pkgInfo{"posplay", posplay.Types, posplay.Functions, posplay.Consts, posplay.Variables})
 	processPkg(pkgInfo{"utils", utils.Types, utils.Functions, utils.Consts, utils.Variables})
 	processPkg(pkgInfo{"website", website.Types, website.Functions, website.Consts, website.Variables})
+	processPkg(pkgInfo{"underlx", Types, Functions, Consts, Variables})
 
 	processPkg(pkgInfo{"discordgo", extpkgDiscordGoTypes, extpkgDiscordGoFunctions, extpkgDiscordGoConsts, extpkgDiscordGoVariables})
 	processPkg(pkgInfo{"uuid", extpkgUUIDTypes, extpkgUUIDFunctions, extpkgUUIDConsts, extpkgUUIDVariables})
+
+	packages["underlx"]["RootSqalxNode"] = func() sqalx.Node {
+		return rootSqalxNode
+	}
+	packages["underlx"]["VehicleHandler"] = func() *compute.VehicleHandler {
+		return vehicleHandler
+	}
+	packages["underlx"]["StatsHandler"] = func() *compute.StatsHandler {
+		return statsHandler
+	}
+	packages["underlx"]["ReportHandler"] = func() *compute.ReportHandler {
+		return reportHandler
+	}
 
 	discordbot.AnkoPackageConfigurator(packages, packageTypes)
 }
