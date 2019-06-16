774d5650656b5e7abaabff6a2523bd689fa1fa7c
gbl08ma <gbl08ma@gmail.com>
2019-06-16 12:54:37 +0100

    underlx.com migration: remove footer and header links, set up redirects

--- website/metapages.go
+++ website/metapages.go
@@ -6,130 +6,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// meta-pages have been moved to underlx.com, now these are just redirects
+
 // AboutPage serves the about page
 func AboutPage(w http.ResponseWriter, r *http.Request) {
-	tx, err := rootSqalxNode.Beginx()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		webLog.Println(err)
-		return
-	}
-	defer tx.Commit()
-
-	p := struct {
-		PageCommons
-	}{}
-
-	p.PageCommons, err = InitPageCommons(tx, w, r, "Sobre nós")
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	err = webtemplate.ExecuteTemplate(w, "about.html", p)
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+	http.Redirect(w, r, "https://underlx.com/about", http.StatusMovedPermanently)
 }
 
 // DonatePage serves the donations page
 func DonatePage(w http.ResponseWriter, r *http.Request) {
-	tx, err := rootSqalxNode.Beginx()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		webLog.Println(err)
-		return
-	}
-	defer tx.Commit()
-
-	p := struct {
-		PageCommons
-	}{}
-
-	p.PageCommons, err = InitPageCommons(tx, w, r, "Donativos")
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	err = webtemplate.ExecuteTemplate(w, "donate.html", p)
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+	http.Redirect(w, r, "https://underlx.com/donate", http.StatusMovedPermanently)
 }
 
 // PrivacyPolicyPage serves the privacy policy page
 func PrivacyPolicyPage(w http.ResponseWriter, r *http.Request) {
-	tx, err := rootSqalxNode.Beginx()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		webLog.Println(err)
-		return
-	}
-	defer tx.Commit()
-
-	p := struct {
-		PageCommons
-	}{}
-
 	if mux.Vars(r)["lang"] != "en" {
-		p.PageCommons, err = InitPageCommons(tx, w, r, "Política de privacidade")
+		http.Redirect(w, r, "https://underlx.com/privacy", http.StatusMovedPermanently)
 	} else {
-		p.PageCommons, err = InitPageCommons(tx, w, r, "Privacy Policy")
-	}
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if mux.Vars(r)["lang"] != "en" {
-		err = webtemplate.ExecuteTemplate(w, "privacy.html", p)
-	} else {
-		err = webtemplate.ExecuteTemplate(w, "privacy-en.html", p)
-	}
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Redirect(w, r, "https://underlx.com/privacy/en", http.StatusMovedPermanently)
 	}
 }
 
 // TermsPage serves the terms and conditions page
 func TermsPage(w http.ResponseWriter, r *http.Request) {
-	tx, err := rootSqalxNode.Beginx()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		webLog.Println(err)
-		return
-	}
-	defer tx.Commit()
-
-	p := struct {
-		PageCommons
-	}{}
-
 	if mux.Vars(r)["lang"] != "en" {
-		p.PageCommons, err = InitPageCommons(tx, w, r, "Termos e Condições")
+		http.Redirect(w, r, "https://underlx.com/terms", http.StatusMovedPermanently)
 	} else {
-		p.PageCommons, err = InitPageCommons(tx, w, r, "Terms")
-	}
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if mux.Vars(r)["lang"] != "en" {
-		err = webtemplate.ExecuteTemplate(w, "terms.html", p)
-	} else {
-		err = webtemplate.ExecuteTemplate(w, "terms-en.html", p)
-	}
-	if err != nil {
-		webLog.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Redirect(w, r, "https://underlx.com/terms/en", http.StatusMovedPermanently)
 	}
 }
